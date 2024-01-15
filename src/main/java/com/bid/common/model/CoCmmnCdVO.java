e8916e58c0516cd29767a6ab276824fd73ea5555
smJeon <xyzp5183@naver.com>
2024-01-15 11:11:00 +0900

    공통코드VO 추가

--- src/main/java/com/bid/common/model/CoCmmnCdVO.java
+++ src/main/java/com/bid/common/model/CoCmmnCdVO.java
@@ -9,35 +9,35 @@ import lombok.Data;
 **/
 @Data
 public class CoCmmnCdVO {
-    private String mainCode;
-    private String subCode;
-    private String extrlSysMainCode;
-    private String codeNm;
-    private int codeLt;
-    private int codeOrdr;
-    private String codeDcone;
-    private String codeDctwo;
-    private String codeRefrnone;
-    private String codeRefrntwo;
-    private String codeRefrnthree;
-    private String codeChrctrRefrnone;
-    private String codeChrctrRefrntwo;
-    private String codeChrctrRefrnthree;
-    private String codeChrctrRefrnfour;
-    private String codeChrctrRefrnfive;
-    private String codeChrctrRefrnsix;
-    private String codeNumberRefrnone;
-    private String codeNumberRefrntwo;
-    private String codeNumberRefrnthree;
-    private String codeNumberRefrnfour;
-    private String sysAt;
-    private String extrlSysUseAt;
-    private String useAt;
-    private String extrlSysIntrlckAt;
-    private String deleteAt;
-    private String deleteDt;
-    private String frstRegisterId;
-    private String frstRegistDt;
-    private String lastChangerId;
-    private String lastChangeDt;
+    private String mainCode;                    // 메인코드
+    private String subCode;                     // 서브코드
+    private String extrlSysMainCode;            // 외부시스템메인코드
+    private String codeNm;                      // 코드명
+    private int codeLt;                         // 코드길이
+    private int codeOrdr;                       // 코드순서
+    private String codeDcone;                   // 코드설명1
+    private String codeDctwo;                   // 코드설명2
+    private String codeRefrnone;                // 코드참조1
+    private String codeRefrntwo;                // 코드참조2
+    private String codeRefrnthree;              // 코드참조3
+    private String codeChrctrRefrnone;          // 코드문자참조1
+    private String codeChrctrRefrntwo;          // 코드문자참조2
+    private String codeChrctrRefrnthree;        // 코드문자참조3
+    private String codeChrctrRefrnfour;         // 코드문자참조4
+    private String codeChrctrRefrnfive;         // 코드문자참조5
+    private String codeChrctrRefrnsix;          // 코드문자참조6
+    private String codeNumberRefrnone;          // 코드숫자참조1
+    private String codeNumberRefrntwo;          // 코드숫자참조2
+    private String codeNumberRefrnthree;        // 코드숫자참조3
+    private String codeNumberRefrnfour;         // 코드숫자참조4
+    private String sysAt;                       // 시스템여부
+    private String extrlSysUseAt;               // 외부시스템사용여부
+    private String useAt;                       // 사용여부
+    private String extrlSysIntrlckAt;           // 외부시스템연동여부
+    private String deleteAt;                    // 삭제여부
+    private String deleteDt;                    // 삭제일시
+    private String frstRegisterId;              // 최초등록자아이디
+    private String frstRegistDt;                // 최초등록일시
+    private String lastChangerId;               // 최종변경자아이디
+    private String lastChangeDt;                // 최종변경일시
 }
